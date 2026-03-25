ebee024c750a7b8b8664cf53eb640d8a2d017170
nino <pilopilosimon1990@gmail.com>
2026-03-25 11:51:30 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -24,9 +24,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 # Configure emulated_storage.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Configure twrp common.mk
-$(call inherit-product, vendor/twrp/config/common.mk)
-
 # Build bootctl
 PRODUCT_PACKAGES += \
         bootctl
